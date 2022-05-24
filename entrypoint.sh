f732c5e42060a54b38fd8c8cc1c9f07958cb3282
Robin <robin.sharma@innerspace.eu>
2022-05-24 12:47:06 +0200

    Uncomment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,34 @@
-﻿##!/bin/sh -l
-echo "hello"
+﻿#!/bin/bash
 
-#release=$1
-#email=$2
-#name=$3
-#
-#if [ -z "$release" ]
-#then
-#  echo "No release label found, skipping cherry pick"
-#else
-#  commit="${GITHUB_SHA}"
-#  echo "Cherry picking commit with sha: ${commit}"
-#  git remote update
-#  git fetch --all
-#  git checkout "${release}"
-#  git config user.email "${email}"
-#  git config user.name "${name}"
-#  CP=$(git cherry-pick "${commit}")
-#  echo "Cherry pick complete ${CP}"
-#  CONFLICTS=$(git ls-files -u | wc -l)
-#  if [ "$CONFLICTS" -gt 0 ] ; then
-#    echo "There is a merge conflict. Aborting cherry pick."
-#    git cherry-pick --abort
-#    branchName="test"
-#    git branch ${branchName} "${commit}"
-#    git push origin ${branchName}
-#    echo ::set-output name=branch::"${branch-name}"
-#    echo ::set-output name=create-pr::"false"
-#  else
-#    echo "Cherry pick successful, pushing."
-#    git push -u origin "${release}"
-#    echo ::set-output name=create-pr::"true"
-#  fi
-#fi
+release=$1
+email=$2
+name=$3
+
+if [ -z "$release" ]
+then
+  echo "No release label found, skipping cherry pick"
+else
+  commit="${GITHUB_SHA}"
+  echo "Cherry picking commit with sha: ${commit}"
+  git remote update
+  git fetch --all
+  git checkout "${release}"
+  git config user.email "${email}"
+  git config user.name "${name}"
+  CP=$(git cherry-pick "${commit}")
+  echo "Cherry pick complete ${CP}"
+  CONFLICTS=$(git ls-files -u | wc -l)
+  if [ "$CONFLICTS" -gt 0 ] ; then
+    echo "There is a merge conflict. Aborting cherry pick."
+    git cherry-pick --abort
+    branchName="test"
+    git branch ${branchName} "${commit}"
+    git push origin ${branchName}
+    echo ::set-output name=branch-name::"${branchName}"
+    echo ::set-output name=create-pr::"false"
+  else
+    echo "Cherry pick successful, pushing."
+    git push -u origin "${release}"
+    echo ::set-output name=create-pr::"true"
+  fi
+fi
